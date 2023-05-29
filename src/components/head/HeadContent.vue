df96dab70b9050686ecc63a146aa12432252893a
墨宇 <zzh_hz@126.com>
2023-05-29 18:21:36 +0800

    样式

--- src/components/head/HeadContent.vue
+++ src/components/head/HeadContent.vue
@@ -1,41 +1,41 @@
 <template>
-  <div>
-    <div>
-      <form>
-        <label><input name="device_platform" type="radio" value="Android"/> Android <svg-icon name="android" /> </label>
-        <label><input name="device_platform" type="radio" value="iOS"/>
-<!--          <svg-icon icon-class="iOS" />-->
-          <iosSvg width="60" height="60" color="#00ff00"></iosSvg>
-        </label>
-      </form>
-    </div>
-    <div>
-
-    </div>
-    <div>
-
-    </div>
+  <div class="top-menu">
+    <form>
+      <label><input name="device_platform" type="radio" value="json"/>
+        <svg-icon width="30" height="30" color="#c2c2c2" name="json"/>
+      </label>
+      <label><input name="device_platform" type="radio" value="Android"/>
+        <svg-icon name="android" width="30" color="#c2c2c2" height="30"/>
+      </label>
+      <label><input name="device_platform" type="radio" value="iOS"/>
+        <svg-icon width="30" height="30" color="#c2c2c2" name="iOS"/>
+      </label>
+    </form>
   </div>
 </template>
 
 <script>
-import iosSvg from '@/components/svg/iosSvg'
+
 import SvgIcon from "@/SvgIcon/index.vue";
 
 export default {
   name: "HeadContent",
-  components: {SvgIcon, iosSvg},
+  components: {SvgIcon},
   props: {
     tableLabel: {}
   },
   data(){
     return {
-      iosSvg: iosSvg
+
     }
   }
 }
 </script>
 
 <style scoped>
-
+.top-menu{
+  position: absolute;
+  top: 0;
+  left: 0;
+}
 </style>
